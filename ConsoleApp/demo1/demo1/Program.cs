3bacfbc9c371de01f1079905a10dc21cad1738d7
antlerjely96 <antlerjely96@gmail.com>
2023-04-28 09:22:55 +0700

    add demo consoleApp

---
+++ ConsoleApp/demo1/demo1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Nhập số nguyên a. Kiểm tra a là số âm hay số dương
+
+//Khai báo a
+int a;
+//Nhập a
+a = int.Parse(Console.ReadLine());
+//Kiểm tra a
+if (a > 0)
+{
+    Console.WriteLine(a + " la so duong");
+}
+else if (a < 0)
+{
+    Console.WriteLine(a + " la so am");
+}
+else
+{
+    Console.WriteLine(a + " la so khong am khong duong");
+}
